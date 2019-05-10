54fc6acebabfe7d74cb1e2f7cc9d79753004c310
Rodrigo Alvarenga <ralvarenga@red4g.net>
2019-05-09 19:34:09 -0600

    Created index file with get method function and allowed headers

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+const app = express();
+const port = 3000;
+import { getAlbum } from "./gphotos";
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/**
+ * @Function
+ * Defines a route handle for the default home page
+ */
+// Defines a route handler for the default home page
+app.get("/:id", async (req, res) => {
+  try {
+    const results = await getAlbum(req.params.id);
+    res.json(results);
+  } catch (e) {
+    res.status(500);
+  }
+});
+
+/**
+ * @Function
+ * Starts the express Server
+ */
+app.listen( port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http:localhost:${port}`);
+});
