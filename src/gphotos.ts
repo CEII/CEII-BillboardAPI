6df154dbb3e4f00daa5f71fada4bc4d1944c0c3e
Rodrigo Alvarenga <ralvarenga@red4g.net>
2019-05-09 19:34:53 -0600

    Adding functions to fetch to google Photos API, and obtain the photos collection via axios

---
+++ src/gphotos.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from "axios";
+
+const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g;
+
+function obtainPhotos(content: string): Array<Set<any>> {
+  const links: Set<any> = new Set();
+  const match: any = null;
+  while (match === regex.exec(content)) {
+    links.add(match[1]);
+  }
+  return Array.from(links);
+}
+
+export async function getAlbum(id: number) {
+  try {
+    const response: AxiosResponse = await axios.get(`https://photos.app.goo.gl/${id}`);
+    return obtainPhotos(response.data);
+  } catch (e) {
+    return null;
+  }
+}
